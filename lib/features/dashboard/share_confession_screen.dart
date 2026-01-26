9ae6f1268bc4cd7e0b45468648fa12264c853252
Ansh Malik <thisisanshmalik@gmail.com>
2026-01-26 22:10:03 +0530

    added components

---
+++ lib/features/dashboard/share_confession_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShareConfessionScreen extends StatelessWidget {
+  const ShareConfessionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
